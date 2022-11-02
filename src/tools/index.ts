9a7882c129f8308cc1901f6ddb31f1614931dfd6
Artem Reva <artem.reva@inspirebrands.com>
2022-11-02 20:22:21 +0300

    Parse MD file

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -1,5 +1,8 @@
 import { Langs } from "hooks/types";
 
+const QUESTION_PREFIX = "####";
+const ANSWER_PREFIX = "- [";
+
 export const getCookie = (cname: string): string => {
   let name = cname + "=";
   let decodedCookie = decodeURIComponent(document.cookie);
@@ -26,9 +29,56 @@ export function setCookie(cname: string, cvalue: Langs, exdays: number | null = 
   document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
-export function parse(lines: string) {
+interface IQuestion {
+  question: string;
+  description: string[];
+  answers: string[];
+  explanation: string[];
+}
+
+enum EMode {
+  question,
+  description,
+  answers,
+  explanation,
+}
+
+export function parse(lines: string): IQuestion[] {
   const linesArr = lines.split("\n");
+  const questions: IQuestion[] = [];
+  let question: IQuestion | null = null;
+  let mode = EMode.question;
+
   linesArr.forEach((rawline) => {
     const line = rawline.trim();
+    const isQuestions = (line: string): boolean => line.substring(0, 4) === QUESTION_PREFIX;
+    const isAnswer = (line: string): boolean => line.substring(0, 3) === ANSWER_PREFIX;
+
+    if (isQuestions(line)) {
+      question && questions.push(question);
+      question = {
+        question: line.substring(5),
+        explanation: [],
+        answers: [],
+        description: [],
+      };
+      mode = EMode.question;
+    }
+
+    if (isAnswer(line)) {
+      question && question.answers.push(line);
+    }
+
+    if (!isAnswer(line) && !isQuestions(line)) {
+      if (mode === EMode.question) question?.explanation.push(line);
+      if (mode === EMode.answers && question)
+        question.answers[question.answers.length - 1] += "\n" + line;
+      if (mode === EMode.answers && line === "") {
+        mode = EMode.explanation;
+      }
+      if (mode === EMode.explanation) question?.explanation.push(line);
+    }
   });
+  question && questions.push(question);
+  return questions;
 }
