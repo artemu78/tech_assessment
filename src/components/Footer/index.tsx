ee93e948072e17d5688f4281c37105c06c3f5391
Artem Reva <artemu78@yandex.ru>
2022-11-06 22:27:53 +0300

    Add header badge, add footer link

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,10 +1,12 @@
 const Footer = (): JSX.Element => {
   return (
     <footer style={{ backgroundColor: "lightgrey" }}>
-      Technical assessment,{" "}
       <a href="https://github.com/Ebazhanov/linkedin-skill-assessments-quizzes">questions source</a>
-      , <a href="https://github.com/artemu78/tech_assessment">source repository</a>,{" "}
-      <a href="https://artemu78.github.io/tech_assessment/coverage-ts/">typescript coverage</a>
+      , <a href="https://github.com/artemu78/tech_assessment">this app source repository</a>,{" "}
+      <a href="https://artemu78.github.io/tech_assessment/coverage-ts/">
+        this app typescript coverage
+      </a>
+      , <a href="https://t.me/artemu78">hire/contact me</a>
     </footer>
   );
 };
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,68 @@
+import { useState } from "react";
+import Badge, { BadgeProps } from "@mui/material/Badge";
+import Dialog from "@mui/material/Dialog";
+import { styled } from "@mui/material/styles";
+import InfoIcon from "@mui/icons-material/Info";
 import styles from "./styles.module.css";
 
+const StyledBadge = styled(Badge)<BadgeProps>(({ theme }) => ({
+  "& .MuiBadge-badge": {
+    right: -10,
+    top: 13,
+    backgroundColor: "transparent",
+  },
+}));
+
 const Header = (): JSX.Element => {
+  const [isOpen, setOpen] = useState(false);
+
+  const handleClick = () => {
+    setOpen((st) => !st);
+  };
+
   return (
     <header>
       <a href="https://war.ukraine.ua/support-ukraine/" target="_blank" rel="noreferrer">
         <div className={styles.praporBlue} />
         <div className={styles.praporYellow} />
       </a>
-      <h1>Technical Assessment</h1>
+      <StyledBadge
+        badgeContent={<InfoIcon htmlColor="#404040" />}
+        color="primary"
+        onClick={handleClick}
+      >
+        <h1>Technical Assessment</h1>
+      </StyledBadge>
+      <Dialog onClose={handleClick} open={isOpen}>
+        <div>
+          This is open source technical questions assessment.
+          <br />
+          <br />
+          Choose theme. Answer questions one by one or choose one you feel comfortable with.
+          <br />
+          You can find your right and wrong answers counted.
+          <br />
+          <br />
+          Questions and answers - are open source community baked{" "}
+          <a
+            href="https://github.com/ebazhanov/linkedin-skill-assessments-quizzes/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            repository
+          </a>
+          . You see a fresh version every time you load page.
+          <br />
+          Feel free to{" "}
+          <a
+            href="https://docs.github.com/en/get-started/exploring-projects-on-github/finding-ways-to-contribute-to-open-source-on-github"
+            target="_blank"
+            rel="noreferrer"
+          >
+            contribute
+          </a>
+        </div>
+      </Dialog>
     </header>
   );
 };
