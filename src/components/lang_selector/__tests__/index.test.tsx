d32bd341a33e7d17c61ebd882d99aead358bbd8d
artemu78 <artemreva79@gmail.com>
2023-12-18 19:10:12 +0000

    Deploying to gh-pages from @ artemu78/tech_assessment@f4c2913136ec32f6e46f0d13fc7ea1a3bbf0900d 🚀

--- src/components/lang_selector/__tests__/index.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from "@testing-library/react";
-import LangSelector from "..";
-
-describe("Header", () => {
-  test("Render snapshot", () => {
-    const { container } = render(<LangSelector />);
-    expect(container).toMatchSnapshot();
-  });
-});
